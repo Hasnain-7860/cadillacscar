767701aac62935d5c89058a87ab152ad7e9939e9
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-03-13 17:06:35 +0530

    .

--- src/components/Feauture.jsx
+++ src/components/Feauture.jsx
@@ -22,13 +22,13 @@ const Feauture = () => {
 
             </ul>
         </div>
-        <div className='   lg:border-solid border-2 border-white shadow-lg mt-[20px] mb-[20px]'>
-           <div className='px-[10px] lg:flex flex-col justify-center items-center px-[200px] py-[50px]'>
+        <div className='   lg:border-solid border-2 border-white shadow-lg mt-[20px] mb-[20px] w-[100%]'>
+           <div className=  ' flex flex-col justify-center items-center  py-[50px]'>
             <h1 className='text-[20px]'>2025</h1>
             <h1 className='text-[25px]'>LYRIQ</h1>
-            <h1 className='text-[50px] '>2.9% APR FOR 48 MONTHS*</h1>
+            <h1 className=' text-[30px] lg:text-[50px] '>2.9% APR FOR 48 MONTHS*</h1>
             <p className='text-[30px]'>FOR WELL-QUALIFIED BUYERS</p>
-            <button className='border-solid border-2 border-white shadow-lg rounded-md p-5  px-[150px] mt-8'>SEARCH INVENTORY</button>
+            <button className=' px-[50px] p-1 lg:border-solid border-2 border-white shadow-lg rounded-md p-5  px-[150px] mt-8'>SEARCH INVENTORY</button>
             <button className='flex justify-center items-center gap-10 mt-8 text-[20px]'> <h1>VIEW  ALL LYRIQ OFFERS</h1> <FaArrowRightLong />
             </button>
         </div>
