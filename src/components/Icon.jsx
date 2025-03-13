1c139ba6222ef1ffe2b1170a12c2133854e05afe
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-03-13 19:50:30 +0530

    ..

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -6,18 +6,16 @@ import { div } from 'motion/react-client'
 const Icon = () => {
   return (
     <div className='bg-gray-200'>
-        <div className='flex justify-center items-center text-[40px] py-[15px]'>
+        <div className='flex justify-center items-center text-[25px] py-[15px] lg:text-[40px]'>
             <h1>FIND YOUR ICON</h1>
         </div>
         <div>
-            <div className='grid  grid-cols-1 gap-10 lg:grid-cols-4 container justify-center items-center lg:gap-40 '>
+            <div className='grid  grid-cols-1   lg:grid-cols-4 container py-10'>
                 {IconData.map((data)=>(
                     <div key={data.id} >
-                        <div className='flex flex-col p-[50px] bg-gray-200 justify-center items-center border-solid border-2 border-black text-[20px] font-semibold '>
-                        <data.data/>
+                        <div className='flex flex-col p-[50px] bg-gray-200 justify-center items-center border-solid border-2 border-black text-[20px] font-semibold gap-8 mt-10 lg:mr-10 '>
+                       <h1 className='text-[40px]'> <data.data/></h1>
                         <h1 className='text-[20px] '>{data.name}</h1>
-                          
-                        
                         </div>
                     </div>
                 ))}
