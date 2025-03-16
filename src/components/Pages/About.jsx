a5f32e27f975559f4e17a52b8f607b9660040c73
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-03-17 03:57:45 +0530

    ....

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <h1>Contact Us</h1>
+        <p>Fill out the form below and we'll contact you shortly.</p>
+      </div>
+      <div>
+        <form >
+          <label>
+          *First Name
+          </label>
+          <input type="text" name='name' placeholder='First Name' />
+          <label>
+          *Last Name
+          </label>
+          <input type="text" name='last name' placeholder='Last Name' />
+          <lable>*E-Mail Address</lable>
+          <input type="text" name='email' placeholder='Email Address' />
+          <label>*Phone</label>
+          <input type="text" name='phone' placeholder='Phone Number' />
+          <label>Comments:</label>
+          <textarea name='Comment' rows="6"></textarea>
+        </form>
+        <div>
+          <h1>Phone</h1>
+          <p>Sales: 352-702-0832</p>
+          <p>Service: 352-702-0832</p>
+          <p>Parts: 352-702-0667</p>     
+           </div>
+
+      </div>
+    </div>
   )
 }
 
