7d62474c1d01b7edad6ed6aed17ba953dd9ba6c7
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-03-16 16:19:42 +0530

    ...

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {ExploreData} from "../components/MockData/data"
 import { div } from 'motion/react-client'
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
+import './Explore.css'
 
 
 
 
 const Explore = () => {
+  const slider =useRef();
+  let tx=0;
+
+  const slideBackward =()=>{
+    if(tx < 0){
+      tx +=15
+    }
+    slider.current.style.transform =`translateX(${tx}%)`;
+
+  };
+  const slideForward =()=>{
+    if(tx > -70){
+      tx -=15
+    }
+    slider.current.style.transform =`translateX(${tx}%)`;
+  }
   return (
     <div className=' flex flex-col justify-center items-center'>
         <div>
@@ -24,42 +41,109 @@ const Explore = () => {
                 
             </ul>
         </div>
-        {/* <div className='bg-black flex overflow-auto w-[100%] h-[300px] '>
-          {
-            ExploreData.map((data)=>(
-              <div  key={data.id}>
-                <div className=''>
-                  <img   src={data.img} alt="" />
-                  <h1 className=''>{data.name}</h1>
-                  <p>{data.text}</p>
-                </div>
-              </div>
-            ))
-          } */}
-          {/* </div> */}
-           
-            <div className=''>
-              <div className='flex w-[100%] h-[400px] overflow-auto  display-none  '>
-              
 
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/07-images/lineup-suvs-26-escalade-iql-l.png?imwidth=3000'   alt="" />
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-25-escalade-iq-l-v6.png?imwidth=3000'   alt="" />
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-sedans-celestiq-l-v4.png?imwidth=3000'   alt="" />
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-26-vistiq-l-v2.png?imwidth=3000'   alt="" />
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-26-lyriq-v-l.png?imwidth=3000'   alt="" />
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-25-lyriq-l-v3.png?imwidth=3000'   alt="" />
-                <img src='https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-25-optiq-l-v3.png?imwidth=3000'   alt="" />
- 
- 
-                
-              </div>
-              <div className='  z-10 p-[5vh]  flex  items-center gap-5  justify-center  '>
-                <a className=''  href=""><IoIosArrowBack className='text-[40px] border-solid border-2 border-black ' /></a>
-                <a href=""><IoIosArrowForward className='text-[40px] border-solid border-2 border-black' />
-                </a>
+            <div className='overflow-hidden'>
+             <ul id='car' ref={slider} className='flex w-[240%] overflow-x-hidden '>
+                   {ExploreData.map((data)=>(
+                    
+                     <div key={data.id} >
+                      
+                      
+                      <img src={data.img} alt="" />
+                      {data.name}
+                     <h1> {data.text}</h1>
+                      </div>
+                     
+        
+                 
+                     ))}
+                     </ul>
+                     </div>
+               
+          
+       
+                {/* <div  className='overflow-hidden '> 
+                  <ul id='car' ref={slider} className='flex w-[600%] overflow-x-hidden lg:w-[240%] '>
+
+               
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/07-images/lineup-suvs-26-escalade-iql-l.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-25-escalade-iq-l-v6.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-sedans-celestiq-l-v4.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-26-vistiq-l-v2.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-26-lyriq-v-l.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-25-lyriq-l-v3.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <div>
+                        <img src="https://www.cadillac.com/content/dam/cadillac/na/us/english/index/home-page-refresh/lineup/05-images/lineup-suvs-25-optiq-l-v3.png?imwidth=3000" alt="" />
+                      </div>
+                      <h3>dfjgti fhrj hfr</h3>
+                      <span>fjrhv hasnain</span>
+                    </div>
+                  </li>
+                </ul>
+              </div> */}
+
+           
+                <div className='flex gap-5'><a onClick={slideBackward} ><IoIosArrowBack  className='text-[40px] border-solid border-2 border-black ' /></a>
+                <a onClick={slideForward} ><IoIosArrowForward className='text-[40px] border-solid border-2 border-black' /></a>
+               
                 </div>
+
+               
+                
+            
+             
             </div>
-    </div>
+    
    
   )
 }
